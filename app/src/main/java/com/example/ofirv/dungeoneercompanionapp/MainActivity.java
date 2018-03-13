21b920744b09efa0fbeba34daa3e7c647c663fa6
ofirvard <ofirvard55@gmail.com>
2018-03-13 22:55:30 +0200

    0.4

--- app/src/main/java/com/example/ofirv/dungeoneercompanionapp/MainActivity.java
+++ app/src/main/java/com/example/ofirv/dungeoneercompanionapp/MainActivity.java
@@ -7,157 +7,121 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-public class MainActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+public class MainActivity extends AppCompatActivity
+{
+    int level = 0, health = 0, strength = 0, magic = 0, action = 0, glory = 0, peril = 0;
+    TextView levelText, healthText, strengthText, magicText, actionText, gloryText, perilText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
-
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        levelText = findViewById(R.id.level);
+        healthText = findViewById(R.id.health);
+        strengthText = findViewById(R.id.strength);
+        magicText = findViewById(R.id.magic);
+        actionText = findViewById(R.id.action);
+        gloryText = findViewById(R.id.glory);
+        perilText = findViewById(R.id.peril);
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    public void change(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.level_plus_one:
+                level++;
+                levelText.setText(String.format("%02d", level));
+                break;
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
+            case R.id.level_minus_one:
+                level--;
+                levelText.setText(String.format("%02d", level));
+                break;
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
+            case R.id.health_plus_one:
+                health++;
+                healthText.setText(String.format("%02d", health));
+                break;
+
+            case R.id.health_minus_one:
+                health--;
+                healthText.setText(String.format("%02d", health));
+                break;
+
+            case R.id.strength_plus_one:
+                strength++;
+                strengthText.setText(String.format("%02d", strength));
+                break;
+
+            case R.id.strength_minus_one:
+                strength--;
+                strengthText.setText(String.format("%02d", strength));
+                break;
+
+            case R.id.magic_plus_one:
+                magic++;
+                magicText.setText(String.format("%02d", magic));
+                break;
+
+            case R.id.magic_minus_one:
+                magic--;
+                magicText.setText(String.format("%02d", magic));
+                break;
+
+            case R.id.action_plus_one:
+                action++;
+                actionText.setText(String.format("%02d", action));
+                break;
+
+            case R.id.action_minus_one:
+                action--;
+                actionText.setText(String.format("%02d", action));
+                break;
+
+            case R.id.glory_plus_one:
+                glory++;
+                gloryText.setText(String.format("%02d", glory));
+                break;
+
+            case R.id.glory_minus_one:
+                glory--;
+                gloryText.setText(String.format("%02d", glory));
+                break;
+
+            case R.id.glory_plus_five:
+                glory+=5;
+                gloryText.setText(String.format("%02d", glory));
+                break;
+
+            case R.id.glory_minus_five:
+                glory-=5;
+                gloryText.setText(String.format("%02d", glory));
+                break;
+
+            case R.id.peril_plus_one:
+                peril++;
+                perilText.setText(String.format("%02d", peril));
+                break;
+
+            case R.id.peril_minus_one:
+                peril--;
+                perilText.setText(String.format("%02d", peril));
+                break;
+
+            case R.id.peril_plus_five:
+                peril+=5;
+                perilText.setText(String.format("%02d", peril));
+                break;
+
+            case R.id.peril_minus_five:
+                peril-=5;
+                perilText.setText(String.format("%02d", peril));
+                break;
         }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in delay milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 }
