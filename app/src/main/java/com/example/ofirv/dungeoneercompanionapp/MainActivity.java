c7fd42dccafe6c24947f67dff2c3fe0928645e6c
ofirvard <ofirvard55@gmail.com>
2018-03-27 17:00:53 +0300

    0.7

--- app/src/main/java/com/example/ofirv/dungeoneercompanionapp/MainActivity.java
+++ app/src/main/java/com/example/ofirv/dungeoneercompanionapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    int level = 0, health = 0, strength = 0, magic = 0, action = 0, glory = 0, peril = 0;
+    int level = 1, health = 6, glory = 0, peril = 0;
     TextView levelText, healthText, strengthText, magicText, actionText, gloryText, perilText;
 
     @Override
@@ -25,99 +25,83 @@ public class MainActivity extends AppCompatActivity
         perilText = findViewById(R.id.peril);
     }
 
-    public void change(View view)
+    public void changeLevel(View view)
     {
         switch (view.getId())
         {
             case R.id.level_plus_one:
                 level++;
-                levelText.setText(String.format("%02d", level));
                 break;
 
             case R.id.level_minus_one:
-                level--;
-                levelText.setText(String.format("%02d", level));
+                if (level > 1) level--;
                 break;
+        }
+        levelText.setText(String.format("%02d", level));
+    }
 
+    public void changeHealth(View view)
+    {
+        switch (view.getId())
+        {
             case R.id.health_plus_one:
                 health++;
-                healthText.setText(String.format("%02d", health));
                 break;
 
             case R.id.health_minus_one:
-                health--;
-                healthText.setText(String.format("%02d", health));
+                if (health > 0) health--;
                 break;
+        }
+        healthText.setText(String.format("%02d", health));
+    }
 
-            case R.id.strength_plus_one:
-                strength++;
-                strengthText.setText(String.format("%02d", strength));
-                break;
-
-            case R.id.strength_minus_one:
-                strength--;
-                strengthText.setText(String.format("%02d", strength));
-                break;
-
-            case R.id.magic_plus_one:
-                magic++;
-                magicText.setText(String.format("%02d", magic));
-                break;
-
-            case R.id.magic_minus_one:
-                magic--;
-                magicText.setText(String.format("%02d", magic));
-                break;
-
-            case R.id.action_plus_one:
-                action++;
-                actionText.setText(String.format("%02d", action));
-                break;
-
-            case R.id.action_minus_one:
-                action--;
-                actionText.setText(String.format("%02d", action));
-                break;
-
+    public void changeGlory(View view)
+    {
+        switch (view.getId())
+        {
             case R.id.glory_plus_one:
                 glory++;
-                gloryText.setText(String.format("%02d", glory));
                 break;
 
             case R.id.glory_minus_one:
                 glory--;
-                gloryText.setText(String.format("%02d", glory));
                 break;
 
             case R.id.glory_plus_five:
-                glory+=5;
-                gloryText.setText(String.format("%02d", glory));
+                glory += 5;
                 break;
 
             case R.id.glory_minus_five:
-                glory-=5;
-                gloryText.setText(String.format("%02d", glory));
+                glory -= 5;
                 break;
+        }
+        if (glory < 0)
+            glory = 0;
+        gloryText.setText(String.format("%02d", glory));
+    }
 
+    public void changePeril(View view)
+    {
+        switch (view.getId())
+        {
             case R.id.peril_plus_one:
                 peril++;
-                perilText.setText(String.format("%02d", peril));
                 break;
 
             case R.id.peril_minus_one:
                 peril--;
-                perilText.setText(String.format("%02d", peril));
                 break;
 
             case R.id.peril_plus_five:
-                peril+=5;
-                perilText.setText(String.format("%02d", peril));
+                peril += 5;
                 break;
 
             case R.id.peril_minus_five:
-                peril-=5;
-                perilText.setText(String.format("%02d", peril));
+                peril -= 5;
                 break;
         }
+        if (peril < 0)
+            peril = 0;
+        perilText.setText(String.format("%02d", peril));
     }
 }
